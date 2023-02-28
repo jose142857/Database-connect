69cab7cfd7c89aa454ef9b1c0adaf33f2eeb755e
Van Thi <nguyenvanthi168@gmail.com>
2023-02-28 17:55:31 +0900

    Created database

---
+++ src/main/java/vanthi/com/ThiJavaApplication.java
@@ -0,0 +1,13 @@
+package vanthi.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThiJavaApplication.class, args);
+    }
+
+}
